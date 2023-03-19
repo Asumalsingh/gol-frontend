9bbcfdbff98ece82d256c8e998cf3f8d3b755c1a
Asumalsingh <codewithasu@gmail.com>
2023-03-19 09:09:11 +0530

    close nav on click login/signup added

--- components/Navbar.js
+++ components/Navbar.js
@@ -106,10 +106,10 @@ export default function Navbar() {
             <div className="flex  justify-center text-center">
               <ul className="lg:flex items-center">
                 <li className="capitalize cursor-pointer text-primary-color">
-                  <Link href="/login">Login</Link>
+                  <Link href="/login" onClick={() => setNav(false)}>Login</Link>
                 </li>
                 <li className="capitalize cursor-pointer bg-primary-color text-white mt-3 px-4 py-2 rounded-full">
-                  <Link href="/signUp">Sign Up</Link>
+                  <Link href="/signUp" onClick={() => setNav(false)}>Sign Up</Link>
                 </li>
               </ul>
             </div>
